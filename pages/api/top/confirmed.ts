5a3b6c3f5e22600f927f8444e461efb1282f8e17
Ro11ingButler <rollingkai1214@gmail.com>
2023-04-17 19:07:02 +0800

    Add checkpoint for prisma switch

--- pages/api/top/confirmed.ts
+++ pages/api/top/confirmed.ts
@@ -15,21 +15,26 @@ export default async function asynchandler(req: NextApiRequest, res: NextApiResp
             console.log(error)
         })
 
-    const result = await AppDataSource
-        .getRepository(CovidObservations)
-        .createQueryBuilder("covid")
-        .select("covid.country_region as country")
-        .addSelect("SUM(covid.confirmed) as confirmed")
-        .addSelect("SUM(covid.deaths) as deaths")
-        .addSelect("SUM(covid.recovered) as recovered")
-        .where("DATE(covid.observation_date) = :date", { date: dateString })
-        .groupBy("covid.country_region")
-        .orderBy("confirmed", "DESC")
-        .limit(limit)
-        .getRawMany()
+    try {
+        const result = await AppDataSource
+            .getRepository(CovidObservations)
+            .createQueryBuilder("covid")
+            .select("covid.country_region as country")
+            .addSelect("SUM(covid.confirmed) as confirmed")
+            .addSelect("SUM(covid.deaths) as deaths")
+            .addSelect("SUM(covid.recovered) as recovered")
+            .where("DATE(covid.observation_date) = :date", { date: dateString })
+            .groupBy("covid.country_region")
+            .orderBy("confirmed", "DESC")
+            .limit(limit)
+            .getRawMany()
 
-    res.status(200).json({
-        observation_date: dateString,
-        countries: result
-    })
+        res.status(200).json({
+            observation_date: dateString,
+            countries: result
+        })
+    } catch (err: any) {
+        res.status(400).json({ errors: err.message })
+    }
+
 }
